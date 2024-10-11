33a25ce83c547d077339337741f92be1b6e7e7a5
M. Kepler <mmkepler@yahoo.com>
2024-10-11 13:56:20 -0600

    feat: basic landing logo

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import "../styles/landing.css"
+import { StaticImage } from "gatsby-plugin-image"
+
 
 export default function Landing() {
+  
   return (
-    <div>Landing</div>
+    <div id="landing">
+      <StaticImage id="logo" src="../images/newestlogo2.png" alt="Logo for Melissa Kepler, it has a planet with a small moon orbiting it in the back of the text. " loading="eager"/>
+    </div>
   )
 }
