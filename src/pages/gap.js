3d2ed6cc2348def578fe569c5ca0507a74e29b16
M. Kepler <mmkepler@yahoo.com>
2024-10-15 18:08:56 -0600

    feat: gap page placeholder setup

--- src/pages/gap.js
+++ src/pages/gap.js
@@ -1,9 +1,36 @@
-import * as React from "react"
+import * as React from "react" 
+import * as gapStyles from "../styles/gap.module.css"
+import * as sectionStyles from "../styles/sections.module.css"
+import * as styles from "../styles/home.module.css"
+import { useState } from "react"
+import Header from "../components/Header"
+import Menu from "../components/Menu"
+import Layout from "../components/Layout"
+import Footer from "../components/Footer"
+import { StaticImage } from "gatsby-plugin-image"
+
 
 const GapPage = () => {
+
+
+  const [menuState, setMenuState] = useState(false);
+  //toggle hamburger menu
+  const toggleMenu = (e) => {
+    e.preventDefault();
+    let buttonMenu = document.getElementsByClassName("item");
+    // console.log("toggle", buttonMenu);
+    buttonMenu.ariaExpanded = !menuState;
+    setMenuState(!menuState);
+  }
+
   return (
-    <main>
-      <h1>Gap</h1>
+    <main className={styles.main}>
+      <Header onClick={(e) => toggleMenu(e)}/>
+      {menuState ? <Menu onClick={(e) => toggleMenu(e)} /> : ""}
+      <Layout>
+        <h1>Gap</h1>
+      </Layout>
+      <Footer/>
     </main>
   )
 }
