e3caafcf325ba064e2457f1e202b9bb9d0dad0c6
M. Kepler <mmkepler@yahoo.com>
2024-10-16 19:31:02 -0600

    fix: import moved to top of file

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import * as sectionStyles from "../styles/sections.module.css"
+import { StaticImage } from "gatsby-plugin-image"
 const artisticSkills = ["Graphic Design", "Photo Editing", "Photoshop", "Illustrator", "Sketch", "3D Animation/Modeling"]
 const codingSkills = ["JavaScript/ES6+", "React/Hooks", "Redux", "CSS/Flexbox", "Styled Components", "Bootstrap", "Sass", "Node.js", "Express.js", "MongoDB", "Accessibility", "npm", "Git/GitHub", "Firebase DB & Auth", "Webpack/Babel", "Gulp/Grunt", "Rest APIs", "JQuery"]
 const deploymentSkills = ["Cpanel", "Heroku", "AWS/S3", "Digital Ocean", "Netlify"]
 const knowledgeableSkills = ["Next.js", "Workbox/PWA", "Google Analytics", "Apollo/GraphQL", "Keystone.js", "Python/PHP", "WordPress", "SEO", "Cloudflare", "Adobe XD"]
 
-import { StaticImage } from "gatsby-plugin-image"
 
 
 //const allSkills = ["codingSkills", "deploymentSkills", "knowledgeableSkills", "artisticSkills"]
