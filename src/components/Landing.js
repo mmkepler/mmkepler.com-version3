714265c16a6c45e21a8f9b73025a741181f05f02
M. Kepler <mmkepler@yahoo.com>
2024-10-17 19:43:06 -0600

    fix: removed placeholder from landing image

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,7 +7,7 @@ export default function Landing() {
   
   return (
     <div id="landing"  name="home" className={landingStyles.landing}>
-      <StaticImage id="logo" src="../images/newestlogo2.png" alt="Logo for Melissa Kepler, it has a planet with a small moon orbiting it in the back of the text. " loading="eager"/>
+      <StaticImage id="logo" src="../images/newestlogo2.png" alt="Logo for Melissa Kepler, it has a planet with a small moon orbiting it in the back of the text." loading="eager" placeholder="none"/>
     </div>
   )
 }
