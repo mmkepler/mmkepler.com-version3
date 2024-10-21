e1741cd8ab7874cb3b2cd2e741c0b3de76d0985f
M. Kepler <mmkepler@yahoo.com>
2024-10-21 10:11:18 -0600

    fix: achieved scroll between sections with JS and deleted react-scroll to improve performance and accessibility

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,27 @@
 import React from 'react'
-import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from "react-scroll"
 import * as headerStyles from "../styles/header.module.css"
 
 
 export default function Header(props) {
+
+  let scrollAnim = (e) => {
+    e.preventDefault
+    console.log("e", e)
+    let location = e.target.href.match(/\#(.*)/)[1];
+    console.log("location", location);
+    let el = document.getElementById(location);
+    el.scrollIntoView({behavior: "smooth"})
+  }
+
   return (
     <nav className={headerStyles.nav} id="nav">
       <ul id="header" className={headerStyles.header}>
-      <li className={headerStyles.items}  tabIndex="0"><Link  activeClass="active" to="home" smooth={true} offset={-80}>Melissa<br></br>Kepler</Link></li>
-        <li className={headerStyles.items} tabIndex="0"><Link to="about" smooth={true} offset={-80}>About</Link></li>
-        <li className={headerStyles.items} tabIndex="0"><Link to="skills" smooth={true} offset={-80}>Skills</Link></li>
-        <li className={headerStyles.items} tabIndex="0"><Link to="projects" smooth={true} offset={-80} isDynamic={true}>Projects</Link></li>
-        <li className={headerStyles.items} tabIndex="0"><Link to="contact" smooth={true} offset={-80}>Contact</Link></li>
-        <li className={headerStyles.items} tabIndex="0"><a href="/resume" target="_blank" rel="noreferrer" className={headerStyles.resume}>Resume</a></li>
+      <li className={headerStyles.items}  tabIndex="0"><a href="#main" className={headerStyles.scroll} /*onClick={(e) => scrollAnim(e)}*/ >Melissa<br></br>Kepler</a></li>
+        <li className={headerStyles.items} tabIndex="0"><a href="#about" className={headerStyles.scroll} /*onClick={(e) => scrollAnim(e)}*/>About</a></li>
+        <li className={headerStyles.items} tabIndex="0"><a href="#skills" className={headerStyles.scroll} /*onClick={(e) => scrollAnim(e)}*/ >Skills</a></li>
+        <li className={headerStyles.items} tabIndex="0"><a href="#projects" className={headerStyles.scroll} /*onClick={(e) => scrollAnim(e)}*/>Projects</a></li>
+        <li className={headerStyles.items} tabIndex="0"><a href="#contact" className={headerStyles.scroll} /*onClick={(e) => scrollAnim(e)}*/>Contact</a></li>
+        <li className={headerStyles.items} tabIndex="0"><a href="/resume" className={headerStyles.resume} target="_blank" rel="noreferrer">Resume</a></li>
         <li className={headerStyles.toggle}>
           <button className={headerStyles.menu} id="menu" onClick={props.onClick} type="button" aria-expanded="false" aria-label="hamburger menu">
           <span className={headerStyles.hamburger}></span>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,7 +6,7 @@ import { StaticImage } from "gatsby-plugin-image"
 export default function Landing() {
   
   return (
-    <div id="landing"  name="home" className={landingStyles.landing}>
+    <div id="landing"  className={landingStyles.landing}>
       <StaticImage id="logo" src="../images/newestlogo2.png" alt="Logo for Melissa Kepler, it has a planet with a small moon orbiting it in the back of the text." loading="eager" placeholder="none"/>
     </div>
   )
